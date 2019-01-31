2830830f172a4743615b36221a4d73a3c0e1b50b
github username <github email address>
2019-01-30 18:47:54 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,19 +9,14 @@ attr_accessor :name
   def initialize(name)
     @name = name
     @@all << self
-    #binding.pry
   end
 
   def self.clear_all
-    #binding.pry
     @@all = []
   end
 
   def self.all
-    #binding.pry
-    puts @@all.map { |dog| dog.name}
+    puts @@all.map {|dog| dog.name}
   end
 
-  #self.clear_all
-  #binding.pry
 end
